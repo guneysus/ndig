7529b3924ac660873c34b2968824a5eb70e614ee
Ahmed Şeref Güneysu <serefguneysu@gmail.com>
2020-05-17 02:46:16 +0300

    fix

--- publish.sh
+++ publish.sh
@@ -5,7 +5,7 @@ set -ex
 dotnet publish src/ndig.csproj \
 	--configuration Release \
 	--output build \
-	-p:PublishReadyToRun=true \
+	-p:PublishReadyToRun=false \
 	-p:PublishSingleFile=true \
 	-p:PublishTrimmed=true \
 	--self-contained true \
